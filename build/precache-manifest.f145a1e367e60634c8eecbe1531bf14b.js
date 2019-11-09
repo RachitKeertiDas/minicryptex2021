97fc7b738756822ab822bcfc0e09b9fc9b2fed88
adaephonben <ma18btech11011@iith.ac.in>
2019-11-10 02:06:29 +0530

    AntD

---
+++ build/precache-manifest.f145a1e367e60634c8eecbe1531bf14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5df608897711ac58d829959471f31d2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc9a20c0a21e16f3c971",
+    "url": "/static/css/main.bd9112c8.chunk.css"
+  },
+  {
+    "revision": "36845151859a38e7ab01",
+    "url": "/static/js/2.081b9f39.chunk.js"
+  },
+  {
+    "revision": "cc9a20c0a21e16f3c971",
+    "url": "/static/js/main.34731026.chunk.js"
+  },
+  {
+    "revision": "a9d128427ac8bb1cf616",
+    "url": "/static/js/runtime-main.3f611677.js"
+  },
+  {
+    "revision": "d6c382f2f22ad71b99b001f695c7b3b3",
+    "url": "/static/media/07.d6c382f2.png"
+  }
+]);
